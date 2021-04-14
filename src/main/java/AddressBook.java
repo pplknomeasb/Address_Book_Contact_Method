7c8dbcbe102eb15069a6d06e7813841a1301c4d0
kdhital14 <ed.dhital@gmail.com>
2021-04-14 08:30:44 -0700

    implements add contact method

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,50 +1,227 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook {
 
 
-    private List<Contact> contactList;
+    private List<Contact> contactList = new List<Contact>() {
+        @Override
+        public int size() {
+            return 0;
+        }
 
-    public AddressBook(){
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<Contact> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(Contact contact) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends Contact> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends Contact> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public Contact get(int index) {
+            return null;
+        }
+
+        @Override
+        public Contact set(int index, Contact element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, Contact element) {
+
+        }
+
+        @Override
+        public Contact remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<Contact> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<Contact> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<Contact> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    };
+
+    public AddressBook() {
 
     }
 
-    public AddressBook(Boolean seed){
+    public AddressBook(Boolean seed) {
         seedData();
     }
-    public void viewContacts(){
+
+    public void viewContacts() {
 
     }
 
-    public void addNewContact(){
-        System.out.println(" Add a new contact");
+    public void addNewContact() {
+
+        // Prompts user to enter their contacts details
+        System.out.println("Add a new contact");
+
+        // created new Contact and Scanner object
         Contact contact = new Contact();
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
+        // prompts user to enter first name and stores in firstName variable
         System.out.println("Enter First name");
+        String firstName = input.nextLine();
+            if (firstName.equals("")) {
+                System.out.println("Enter the first name again");
+                firstName = input.nextLine();
+            } else {
+                contact.setFirstName(firstName);
+            }
+        // prompts user to enter last name and stores in lastName variable
+        System.out.println("Enter Last name");
+        String lastName = input.nextLine();
+            if( lastName.equals("")){
+                System.out.println("Enter the last name again");
+                lastName = input.nextLine();
+            } else {
+                contact.setLastName(lastName);
+            }
+        System.out.println("Enter mobile number");
+            String phoneMobile = input.nextLine();
+            if(phoneMobile.equals("")){
+                System.out.println("Enter mobile number again");
+                phoneMobile = input.nextLine();
+
+            } else {
+
+                contact.setPhoneMobile(phoneMobile);
+            }
+        System.out.println("Enter email");
+        String email = input.nextLine();
+        contact.setEmail(email);
+
+        System.out.println("Enter street, home number city and zip");
+        String street = input.nextLine();
+        contact.setStreet(street);
+
+        String number = input.nextLine();
+        contact.setNumber(number);
+
+        String city = input.nextLine();
+        contact.setCity(city);
+
+        String zip = input.nextLine();
+        contact.setZip(zip);
+
+        // stored the contact information
+        contactList.add(contact);
+        System.out.println(firstName + "," + lastName+ "," + phoneMobile+ "," +email+ "," +street+ "," +number + "," +city+ "," +zip );
+        System.out.println("Contact information has been saved");
+    }
+
+
+
+    public void deleteContact() {
 
     }
 
-    public void deleteContact(){
-
-    }
-
-    public void searchForContact(){
+    public void searchForContact() {
         System.out.println("Please select search criteria : ");
         System.out.println("1) Search by Name");
         System.out.println("2) Search by Phone Number");
         Scanner input = new Scanner(System.in);
         int option = input.nextInt();
         Contact contact;
-        switch (option){
-            case 1 :
+        switch (option) {
+            case 1:
                 System.out.println("Please enter the First name you want search in address book : ");
                 String name = input.next();
                 contact = search(name, "name");
                 break;
-            case 2 :
+            case 2:
                 System.out.println("Please enter the phone number you want search in address book : ");
                 String phone = input.next();
                 contact = search(phone, "phone");
@@ -56,10 +233,10 @@ public class AddressBook {
 
     private Contact search(String value, String criteria) {
         for (Contact contact : contactList) {
-            if(criteria.equals("name")) {
+            if (criteria.equals("name")) {
                 if (contact.getFirstName().equals(value))
                     return contact;
-            } else if (criteria.equals("phone")){
+            } else if (criteria.equals("phone")) {
                 if (contact.getPhoneMobile().equals(value))
                     return contact;
             }
@@ -68,35 +245,32 @@ public class AddressBook {
     }
 
 
-    public void editContact(){
+    public void editContact() {
 
     }
 
 
-    private void seedData(){
+    private void seedData() {
         contactList = new ArrayList<Contact>();
 
         contactList.add(new Contact("Brennan", "Thompson", "1234567890", "1675309544", "thompson.brennan@gmail.com",
                 "Main St", "112", "Pittsburgh", "15909"));
         contactList.add(new Contact("Trevor", "Benyack", "4125551111", "4125552222", "trb@gmail.com", "Maryland", "1000", "Pittsburgh"
-        , "15232"));
-        contactList.add(new Contact("Greg", "Conti", "7245552468", "4125553888", "gdc@gmail.com", "Palameno", "234", "Gibsonia","15044"));
+                , "15232"));
+        contactList.add(new Contact("Greg", "Conti", "7245552468", "4125553888", "gdc@gmail.com", "Palameno", "234", "Gibsonia", "15044"));
         contactList.add(new Contact("Kedar", "Dhital", "4219992222", "7249990000", "kdd@gmail.com", "Sunny Dr", "142", "Pittsburgh", "15227"));
         contactList.add(new Contact("Ashwini", "Neelgund", "9876554321", "8795462130", "ad@hotmail.com", "per scholas", "413", "Green Tree", "78945"));
     }
 
 
-
-
-    public void readContactFile(){
+    public void readContactFile() {
 
     }
 
 
-    public void writeContactFile(){
+    public void writeContactFile() {
 
     }
-
 
 
 }
