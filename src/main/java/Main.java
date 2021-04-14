48d986d3c207a11ac66e275db235e65a8aedb251
Ashwini <ashwinineelgundh@gmail.com>
2021-04-14 09:28:33 -0400

    Added the basic working search functionality.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        AddressBook book = new AddressBook(true);
+        book.searchForContact();
     }
 }
